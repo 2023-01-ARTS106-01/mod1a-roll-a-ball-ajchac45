c54442b5a60707a4a7d58bb9838bde39b5d165cb
ajchac45 <123001516+ajchac45@users.noreply.github.com>
2023-01-23 12:05:03 -0500

    More Imputs

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponment<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue movementValue)  
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>(); 
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(movementVector);
     }
 }
